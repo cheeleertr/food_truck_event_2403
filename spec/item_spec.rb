0c2a179917dff4aaeb544b9d457f35c111fc0093
Chee Lee <cheeleertr@gmail.com>
2024-04-15 10:07:28 -0500

    Test: add item initialize test

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe do
+  it "initializes" do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(item1).to be_a Item
+    expect(item1.name).to eq('Peach Pie (Slice)')
+    expect(item1.price).to eq("$3.75")
+  end
