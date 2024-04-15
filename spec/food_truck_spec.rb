d3900e89278b2dacfac3f7aacc626578e5b1ec81
Chee Lee <cheeleertr@gmail.com>
2024-04-15 10:35:48 -0500

    Feat: add #check_stock and #stock to FoodTruck class

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     expect(food_truck).to be_a FoodTruck
-    expect(food_truck.name).to eq('Peach Pie (Slice)')
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq({})
   end
 
@@ -33,7 +33,7 @@ RSpec.describe do
       expect(food_truck.inventory).to eq({item1 => 55})
       food_truck.stock(item2, 12)
 
-      expect(food_truck.check_stock(item1)).to eq(item2 => 12)
+      expect(food_truck.check_stock(item2)).to eq(12)
       expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
     end
   end
